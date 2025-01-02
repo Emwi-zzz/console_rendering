46a120e3e5be1c63c204f92e1131553111b56414
Emwi <emwi@fedora.home>
2025-01-02 20:13:19 +0100

    light was implemented

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 int main(){
 
   polygon tr;
-  tr.point_1 = vec3(0, 0 ,0);
-  tr.point_2 = vec3(0, 1, 0);
-  tr.point_3 = vec3(1, 0, 0);
+  tr.point_1 = vec3(0, 1.1, 3);
+  tr.point_2 = vec3(0, 0, 0);
+  tr.point_3 = vec3(1.1, 0, 0);
 
   vector<char> s(WIDTH * HEIGHT, ' ');
   vector<float> z(WIDTH * HEIGHT);
--- render.cpp
+++ render.cpp
@@ -1,4 +1,5 @@
 #pragma once
+#include<math.h>
 #include <algorithm>
 #include<vector>
 #include<iostream>
@@ -38,12 +39,40 @@ class vec3{
     y = b;
     z = c;
   }
+
+  vec3 operator-(const vec3& other) const {
+        return {x - other.x, y - other.y, z - other.z};
+    }
+
+    static vec3 cross(const vec3& a, const vec3& b) {
+        return {
+            a.y * b.z - a.z * b.y,
+            a.z * b.x - a.x * b.z,
+            a.x * b.y - a.y * b.x
+        };
+    }
+
+    static float dot(const vec3& a, const vec3& b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    float norm() const {
+        return std::sqrt(x * x + y * y + z * z);
+    }
+
+    vec3 normalize() const {
+        float magnitude = norm();
+        return {x / magnitude, y / magnitude, z / magnitude};
+    }
+
 };
 
 vec3 project(vec3 v){
   float scale = 1 / ((v.z - CAM_Z) * CAM_F);
   return vec3(((v.x * scale * SCALE) + 1) * WIDTH * 0.5, ((v.y * scale) + 1) * HEIGHT / 2, v.z);
 }
+
+vec3 SunLight = vec3(1, 1, 0).normalize();
 
 class polygon{
   public:
@@ -61,11 +90,35 @@ class polygon{
     cout << p3.x << ' ' << p3.y << ' ' << p3.z << '\n';
 
     cout << "render\n";
+    
+    /*
+     *  D  =  (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3)
+     *
+     *  L1 = ((y2 - y3) * (x  - x3) + (x3 - x2) * (y  - y3)) / D
+     *
+     *  L2 = ((y3 - y1) * (x  - x3) + (x1 - x3) * (y  - y3)) / D 
+     *
+    */
+
+    float D = 1 / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
 
     for(int i = int(min({p1.y, p2.y, p3.y})); i < int(max({p1.y, p2.y, p3.y})); i++){
         for(int j = int(min({p1.x, p2.x, p3.x})); j < int(max({p1.x, p2.x, p3.x})); j++){
-          cout << i << ' ' << j << ' ' << i * WIDTH + j << '\n'; 
-          screen[i * WIDTH + j] = '#';
+          cout << i << ' ' << j << ' ' << i * WIDTH + j << '\n';
+          float L1 = ((p2.y - p3.y) * (j - p3.x) + (p3.x - p2.x) * (i - p3.y)) * D;
+          float L2 = ((p3.y - p1.y) * (j - p3.x) + (p1.x - p3.x) * (i - p3.y)) * D;
+          if((L1 + L2 <= 1) && L1 > 0  && L2 > 0){
+
+            vec3 AB = p2 - p1;
+            vec3 AC = p3 - p1;
+            vec3 normal = vec3::cross(AB, AC).normalize();
+
+            float cosTheta = vec3::dot(normal, SunLight);
+
+            int index = (float)(cosTheta * 13);
+
+            screen[i * WIDTH + j] = ".,-~:;=!*#$@"[index];
+          }
         }
     }
   }
